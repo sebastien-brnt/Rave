7c90c11c7a2f8a0db1eb2e9dcb130629eca73903
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2024-06-04 21:35:47 +0200

    Start adding Audio list

--- components/screens/AudioSelector.js
+++ components/screens/AudioSelector.js
@@ -1,9 +1,38 @@
 import { View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import * as FileSystem from 'expo-file-system';
+
 
 export default function AudioSelector() {
+    // State pour stocker la liste des fichiers audio
+    const [audioFiles, setAudioFiles] = useState([]);
+
+    // Fonction pour récupérer la liste des fichiers audio
+    async function loadAudioFiles() {
+        try {
+            // Path du dossier d'enregistrement
+            const directory = `${FileSystem.documentDirectory}recordings/`;
+            const files = await FileSystem.readDirectoryAsync(directory);
+            setAudioFiles(files);
+        } catch (error) {
+            console.error('Error loading audio files:', error);
+        }
+    }
+
+    // Chargement des fichiers audio
+    useEffect(() => {
+        loadAudioFiles();
+        console.log('Audio files loaded', audioFiles);
+    }, []);
+    
     return (
         <View style={styles.container}>
             <Text>Sélection de l'audio</Text>
+
+            {/* Liste des fichiers audio */}
+            {audioFiles.map((file, index) => (
+                <Text key={index}>{file}</Text>
+            ))}
         </View>
     );
 }
