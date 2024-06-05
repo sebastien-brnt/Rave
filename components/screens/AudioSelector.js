d59948c1e01140babbb2545e78cce772a8de53f7
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2024-06-05 19:26:56 +0200

    Adding delete sound + refresh of sound list

--- components/screens/AudioSelector.js
+++ components/screens/AudioSelector.js
@@ -1,9 +1,13 @@
 import { View, Text } from 'react-native';
 import { useState, useEffect } from 'react';
 import * as FileSystem from 'expo-file-system';
+import CustomButton from '../common/CustomButton';
+import { useNavigation } from '@react-navigation/native';
 
 
 export default function AudioSelector() {
+    const navigation = useNavigation();
+
     // State pour stocker la liste des fichiers audio
     const [audioFiles, setAudioFiles] = useState([]);
 
@@ -19,11 +23,37 @@ export default function AudioSelector() {
         }
     }
 
+    // Fonction pour supprimer un fichier audio
+    async function deleteAudioFile(file) {
+        try {
+            // Path du fichier à supprimer
+            const path = `${FileSystem.documentDirectory}recordings/${file}`;
+            await FileSystem.deleteAsync(path);
+            console.log('File deleted:', file);
+            loadAudioFiles();
+        } catch (error) {
+            console.error('Error deleting audio file:', error);
+        }
+    }
+
     // Chargement des fichiers audio
     useEffect(() => {
         loadAudioFiles();
         console.log('Audio files loaded', audioFiles);
     }, []);
+
+    useEffect(() => {
+        const focusListener = () => {
+            loadAudioFiles();
+        };
+
+        // Ajoutez un listener pour recharger les fichiers lorsque l'écran devient actif
+        const unsubscribe = navigation.addListener('focus', focusListener);
+
+        // Cleanup le listener lors du démontage du composant
+        return unsubscribe;
+    }, [navigation]);
+
     
     return (
         <View style={styles.container}>
@@ -31,7 +61,10 @@ export default function AudioSelector() {
 
             {/* Liste des fichiers audio */}
             {audioFiles.map((file, index) => (
-                <Text key={index}>{file}</Text>
+                <View key={index}>
+                    <Text>{file}</Text>
+                    <CustomButton title="Supprimer" event={() => deleteAudioFile(file)} />
+                </View>
             ))}
         </View>
     );
